57c005537c82cd1ff1125276393e1ab1c744965e
vcarden1 <89755241+vcarden1@users.noreply.github.com>
2022-04-09 13:58:03 -0400

    Update hospital.sql

--- hospital.sql
+++ hospital.sql
@@ -1,12 +1,14 @@
-
 /*patientinfo*/
-insert into Patientinfo values('Arun Sharma','Arun','1991-01-22','M','NJ','US','07307',9876345612,65,'Suffering from cold and cough','2012-06-12 14:00:00');
-insert into Patientinfo values('Vijay Sinha','Vijay','1986-04-12','M','NJ','US','07306',9876275612,70,'High Fever','2013-06-08 15:00:00');
-insert into Patientinfo values('John Cooper','John','1978-06-15','M','NJ','US','0856',9876782612,78,'Throat pain','2014-02-10 18:00:00');
-insert into Patientinfo values('Michelle Stanley','Michelle','1998-08-09','F','NJ','US','07307',9901345612,60,'Stomach ache','2015-05-12 10:00:00');
-insert into Patientinfo values('Ruby Yul','Ruby','1996-03-25','F','NJ','US','03307',3467345612,60,'Heart ache','2016-03-08 11:00:00');
-insert into Patientinfo values('Hanny Khub','Honey','1985-06-20','F','NJ','US','03307',3423445612,72,'Head ache','2016-05-10 11:00:00');
-insert into Patientinfo values('Sameul Honey','Sameul','1988-07-20','M','NJ','US','03307',3421235612,63,'Head ache','2016-05-20 11:00:00');
+insert into Patientinfo values('Arun Sharma','Arun','1991-01-22','M','NJ','US','07307',9876345612,65,'Suffering from cold and cough','2013-06-12 14:00:00', '5061909');
+insert into Patientinfo values('Vijay Sinha','Vijay','1986-04-12','M','NJ','US','07306',9876275612,70,'High Fever','2013-08-09 15:00:00', '5391463');
+insert into Patientinfo values('John Cooper','John','1978-06-15','M','NJ','US','0856',9876782612,78,'Throat pain','2014-03-11 18:00:00', '2889889');
+insert into Patientinfo values('Michelle Stanley','Michelle','1998-08-09','F','NJ','US','07307',9901345612,60,'Stomach ache','2016-05-12 10:00:00', '270335');
+insert into Patientinfo values('Ruby Yul','Ruby','1996-03-25','F','NJ','US','03307',3467345612,60,'Heart ache','2016-03-08 11:00:00', '490794');
+insert into Patientinfo values('Hanny Khub','Honey','1985-06-20','F','NJ','US','03307',3423445612,72,'Head ache','2016-05-10 11:00:00', '1175502');
+insert into Patientinfo values('Sameul Honey','Sameul','1988-07-20','M','NJ','US','03307',3421235612,63,'Head ache','2016-05-20 11:00:00', '219163');
+insert into Patientinfo values('Alexander David','Alex','1986-03-26','M','NJ','US','03307',3468345212,62,'Heart ache','2016-06-28 12:00:00', '9492549');
+insert into Patientinfo values('Hannay Han','Han','1985-06-20','F','NJ','US','03307',3423444512,72,'Head ache','2015-02-10 16:00:00', '661869');
+insert into Patientinfo values('Sammantha Lane','Sam','1988-07-20','M','NJ','US','03307',3421235612,45,'Stomach ache','2015-03-20 17:00:00', '4594720');
 
 /*doctorinfo*/
 insert into Doctorinfo values(101,'Stan Murphy','Stan','M','NJ','US',08234,7623569801,'Jr');
